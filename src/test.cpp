918256031d4f1b5ed80a3e0d07d4b39e0d228848
liuzihan200545 <3069522719@qq.com>
2024-07-31 15:14:27 +0800

    Init

--- src/test.cpp
+++ src/test.cpp
@@ -58,7 +58,6 @@ int main()
     auto vertex = VertexArray(vertices,sizeof(vertices),indices,sizeof(indices),a,3);
 
 
-
     auto vertex1 = VertexArray(vertices,sizeof(vertices),indices,sizeof(indices),a,3);
     while (!glfwWindowShouldClose(window))
     {
